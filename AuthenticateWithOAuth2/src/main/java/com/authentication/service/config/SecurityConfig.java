f2bccb6bd6e9c00d8952cf24843552b112253dff
arturto <tomeyan.artur@gmail.com>
2022-12-30 11:33:47 +0400

    OAuth

--- AuthenticateWithOAuth2/src/main/java/com/authentication/service/config/SecurityConfig.java
+++ AuthenticateWithOAuth2/src/main/java/com/authentication/service/config/SecurityConfig.java
@@ -3,8 +3,10 @@ package com.authentication.service.config;
 import com.authentication.service.service.impl.DefaultOAuth2UserServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
     private final DefaultOAuth2UserServiceImpl defaultOAuth2UserService;
 
@@ -24,18 +27,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/", "/login", "/oauth/**").permitAll()
-                .anyRequest().authenticated()
+        http.authorizeRequests()
+                .antMatchers("/**").fullyAuthenticated()
                 .and()
-                .formLogin().permitAll();
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .oauth2ResourceServer().jwt()
+                .and()
+                .and()
+                .csrf().and().cors().disable();
 
         return http.build();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
--- AuthenticateWithOAuth2/src/main/java/com/authentication/service/controller/AuthenticateController.java
+++ AuthenticateWithOAuth2/src/main/java/com/authentication/service/controller/AuthenticateController.java
@@ -1,5 +1,6 @@
 package com.authentication.service.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthenticateController {
 
+    @GetMapping
+    public String testAuth(){
+        return "Hello";
+    }
 }
